244b75cbcb56c3ce9f0b74445bf226752316062d
poohinajar@gmail.com <poohinajar@gmail.com>
2022-07-20 15:43:08 +0900

    Updates

---
+++ precache-manifest.f494cea0c0d25a0e490d99fd896541d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ff61fecace92f340ecc19077757f1ff",
+    "url": "/recently-item/index.html"
+  },
+  {
+    "revision": "c44e265971cfc43a2c7c",
+    "url": "/recently-item/static/css/main.597a66bb.chunk.css"
+  },
+  {
+    "revision": "2dddeb9e7cf0fa8abf48",
+    "url": "/recently-item/static/js/2.630dc298.chunk.js"
+  },
+  {
+    "revision": "93ff3716d178a079c91c",
+    "url": "/recently-item/static/js/3.ba04eaa6.chunk.js"
+  },
+  {
+    "revision": "c44e265971cfc43a2c7c",
+    "url": "/recently-item/static/js/main.499a8a8e.chunk.js"
+  },
+  {
+    "revision": "50a5fb4dfcc1ef88d77e",
+    "url": "/recently-item/static/js/runtime-main.940beb6c.js"
+  },
+  {
+    "revision": "d316fe2136956901dde04a28001b7b83",
+    "url": "/recently-item/static/media/sp_bt_x.d316fe21.png"
+  },
+  {
+    "revision": "4a3382122f0b7f7797573bfcc47b5020",
+    "url": "/recently-item/static/media/sp_chk.4a338212.png"
+  },
+  {
+    "revision": "be7b3d4cb226a9d2a5db89cadfd17573",
+    "url": "/recently-item/static/media/sp_ico_fn.be7b3d4c.png"
+  }
+]);
